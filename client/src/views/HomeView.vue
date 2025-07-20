809302149059d94d3f1fe31975eed90c599e66b5
spider-hand <creative.spider.hand@gmail.com>
2025-07-20 23:51:13 +0900

    chore: set up tailwindcss and shadcn

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ButtonComponent } from '@/components/ui/button';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ButtonComponent>Click me</ButtonComponent>
 </template>
